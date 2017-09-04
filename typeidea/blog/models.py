ea4a9b6c4ae9d78124cdefca9c6cd21a867afe78
BaskerShu <yangshuzhi@outlook.com>
2017-09-04 21:51:15 +0800

    添加config模块models代码

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=50, verbose_name="标题")
-    desc = models.CharField(max_length=255, balnk=True, verbose_name="摘要")
+    desc = models.CharField(max_length=255, blank=True, verbose_name="摘要")
     category = models.ForeignKey('Category', verbose_name="分类")
     tag = models.ManyToManyField('Tag', verbose_name="标签")
 
