f440330e7bff96a6b3bab90ec9dcfaa010cb89ee
BaskerShu <yangshuzhi@outlook.com>
2017-09-14 22:23:13 +0800

    自定义site并修改blog模块admin中的list页面

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -3,19 +3,25 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from typeidea.custom_site import custom_site
 from .models import Post, Tag, Category
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'category', 'content', 'status', 'owner', 'created_time']
+    list_filter = ['category', 'owner']
+    search_fields = ['title', 'category__name', 'owner__username']
+    show_full_result_count = False
+    list_display_links = ['category', 'status']
+    list_editable = ('title',)
 
 
-@admin.register(Tag)
+@admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Category)
+@admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
     pass
