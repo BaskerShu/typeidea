8efb4d54db511bc7e4961996847332016bc5d8ae
BaskerShu <yangshuzhi@outlook.com>
2017-09-18 22:10:56 +0800

    对blog模块重写admin和form中的方法实现对某些字段的样式进行定制修改

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -5,12 +5,15 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 
-from typeidea.custom_site import custom_site
+from .adminforms import PostAdminForm
 from .models import Post, Tag, Category
+from typeidea.custom_site import custom_site
 
 
 @admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
     list_display = ['title', 'category', 'content', 'status_show',
                     'owner', 'created_time', 'operator']
     list_display_links = []
@@ -22,8 +25,12 @@ class PostAdmin(admin.ModelAdmin):
     save_on_top = False
     fieldsets = (
         ('基础设置', {
-            'fields': (('title', 'category'),
-                       'content', )
+            'fields': (
+                ('title', 'category'),
+                'desc',
+                'status',
+                'content',
+            )
         }),
         ('高级配置', {
             'fields': ('tag',),
@@ -39,6 +46,30 @@ class PostAdmin(admin.ModelAdmin):
         )
     operator.short_description = '操作'
 
+    def save_model(self, request, obj, form, change):
+        # import pdb;pdb.set_trace()
+        obj.owner = request.user
+        super(PostAdmin, self).save_model(request, obj, form, change)
+
+
+class PostInline(admin.StackedInline):
+    fieldsets = (
+        ('基础设置', {
+            'fields': (
+                ('title', 'category'),
+                'desc',
+                'status',
+                'content',
+            )
+        }),
+        ('高级配置', {
+            'fields': ('tag',),
+            'classes': ('collapse', 'addon'),
+        })
+    )
+    extra = 1
+    model = Post
+
 
 @admin.register(Tag, site=custom_site)
 class TagAdmin(admin.ModelAdmin):
@@ -47,4 +78,6 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Category, site=custom_site)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        PostInline,
+    ]
---
+++ typeidea/blog/adminforms.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+    status = forms.BooleanField(label="是否删除", required=False)  # TODO: 处理bool类型为我们需要的字段
+    desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
+
+    def clean_status(self):
+        if self.cleaned_data['status']:
+            return 3
+        else:
+            return 1
