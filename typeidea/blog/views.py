295ea7f014002249f1e7316249969a8a71ea3ac2
baskershu <yangshuzhi@outlook.com>
2017-12-21 07:08:34 -0800

    完成数据的分页功能

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from django.http import Http404
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Tag
 
 
 def post_list(request, category_id=None, tag_id=None):
+    page_size = 2
+    page = request.GET.get('page', 1)
+
     if category_id:
         # 根据分类显示文章列表
         queryset = Post.objects.filter(category_id=category_id)
@@ -22,8 +26,17 @@ def post_list(request, category_id=None, tag_id=None):
     else:
         # 显示所有文章列表
         queryset = Post.objects.all()
+
+    paginator = Paginator(queryset, page_size)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
-        'posts': queryset,
+        'posts': posts,
     }
     return render(request, 'blog/list.html', context)
 
