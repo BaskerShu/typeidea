872eb2cd4fb0f785fa5ad4382cfb85a9ec0b7d9b
baskershu <yangshuzhi@outlook.com>
2017-12-24 22:48:44 -0800

    完成view上的分类导航以及侧边栏显示

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -5,7 +5,9 @@ from django.http import Http404
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from .models import Post, Tag
+from .models import Post, Tag, Category
+from config.models import SideBar
+from comment.models import Comment
 
 
 def post_list(request, category_id=None, tag_id=None):
@@ -14,7 +16,7 @@ def post_list(request, category_id=None, tag_id=None):
 
     if category_id:
         # 根据分类显示文章列表
-        queryset = Post.objects.filter(category_id=category_id)
+        queryset = Post.objects.filter(category_id=category_id, status=1)
     elif tag_id:
         # 根据标签显示文章列表
         try:
@@ -22,11 +24,12 @@ def post_list(request, category_id=None, tag_id=None):
         except Tag.DoesNotExist:
             queryset = []
         else:
-            queryset = tag.post_set.all()
+            queryset = tag.post_set.filter(status=1)
     else:
         # 显示所有文章列表
         queryset = Post.objects.all()
 
+    # 分页
     paginator = Paginator(queryset, page_size)
     try:
         posts = paginator.page(page)
@@ -35,8 +38,28 @@ def post_list(request, category_id=None, tag_id=None):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
+    # 分类导航
+    nav_cates = []
+    cates = []
+    categories = Category.objects.filter(status=1)
+    for cate in categories:
+        if cate.is_nav:
+            nav_cates.append(cate)
+        else:
+            cates.append(cate)
+
+    # 侧边栏
+    side_bars = SideBar.objects.filter(status=1)
+    recently_post = Post.objects.filter(status=1)[:10]
+    recently_comment = Comment.objects.filter(status=1)[:10]
+
     context = {
         'posts': posts,
+        'nav_cates': nav_cates,
+        'cates': cates,
+        'side_bars': side_bars,
+        'recently_post': recently_post,
+        'recently_comment': recently_comment,
     }
     return render(request, 'blog/list.html', context)
 
