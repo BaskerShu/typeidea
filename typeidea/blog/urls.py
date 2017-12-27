c9edeee34cfce57f2f917e4055d326577ee7e224
baskershu <yangshuzhi@outlook.com>
2017-12-27 06:00:33 -0800

    将function view抽象成class base view

--- typeidea/blog/urls.py
+++ typeidea/blog/urls.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from . import views
+from .views import IndexView, CategoryView, TagView, PostView
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='index'),
-    url(r'category/(?P<category_id>\d+)/$', views.post_list, name='category'),
-    url(r'tag/(?P<tag_id>\d+)/$', views.post_list, name='tag'),
-    url(r'post/(?P<post_id>\d+)/$', views.post_detail, name='detail'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category'),
+    url(r'tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag'),
+    url(r'post/(?P<post_id>\d+)/$', PostView.as_view(), name='detail'),
 ]
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,89 +1,78 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import Http404
-from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Post, Tag, Category
 from config.models import SideBar
 from comment.models import Comment
 
 
-def get_common_context():
-    # 分类导航
-    nav_cates = []
-    cates = []
-    categories = Category.objects.filter(status=1)
-    for cate in categories:
-        if cate.is_nav:
-            nav_cates.append(cate)
-        else:
-            cates.append(cate)
+class CommonContextMixin(object):
+    def get_context_data(self, **kwargs):
+        # 分类导航
+        nav_cates = []
+        cates = []
+        categories = Category.objects.filter(status=1)
+        for cate in categories:
+            if cate.is_nav:
+                nav_cates.append(cate)
+            else:
+                cates.append(cate)
 
-    # 侧边栏
-    side_bars = SideBar.objects.filter(status=1)
-    recently_post = Post.objects.filter(status=1)[:10]
-    recently_comment = Comment.objects.filter(status=1)[:10]
+        # 侧边栏
+        side_bars = SideBar.objects.filter(status=1)
+        recently_post = Post.objects.filter(status=1)[:10]
+        recently_comment = Comment.objects.filter(status=1)[:10]
 
-    context = {
-        'nav_cates': nav_cates,
-        'cates': cates,
-        'side_bars': side_bars,
-        'recently_post': recently_post,
-        'recently_comment': recently_comment,
-    }
+        context = {
+            'nav_cates': nav_cates,
+            'cates': cates,
+            'side_bars': side_bars,
+            'recently_post': recently_post,
+            'recently_comment': recently_comment,
+        }
+        context.update(kwargs)
 
-    return context
+        return super(CommonContextMixin, self).get_context_data(**context)
 
 
-def post_list(request, category_id=None, tag_id=None):
-    page_size = 2
-    page = request.GET.get('page', 1)
+class ListPostsView(CommonContextMixin, ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    paginate_by = 2
+    context_object_name = 'posts'
 
-    if category_id:
-        # 根据分类显示文章列表
-        queryset = Post.objects.filter(category_id=category_id, status=1)
-    elif tag_id:
-        # 根据标签显示文章列表
+
+class IndexView(ListPostsView):
+    pass
+
+
+class CategoryView(ListPostsView):
+    def get_queryset(self):
+        queryset = super(CategoryView, self).get_queryset()
+        cate_id = self.kwargs.get('category_id', 1)  # 获得从url传递的category_id
+        posts = queryset.filter(category_id=cate_id, status=1)
+
+        return posts
+
+
+class TagView(ListPostsView):
+    def get_queryset(self):
+        tag_id = self.kwargs.get('tag_id')
         try:
             tag = Tag.objects.get(id=tag_id)
         except Tag.DoesNotExist:
-            queryset = []
+            posts = []
         else:
-            queryset = tag.post_set.filter(status=1)
-    else:
-        # 显示所有文章列表
-        queryset = Post.objects.all()
+            posts = tag.post_set.filter(status=1)
 
-    # 分页
-    paginator = Paginator(queryset, page_size)
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    context = {
-        'posts': posts,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-
-    return render(request, 'blog/list.html', context)
+        return posts
 
 
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        raise Http404("post does not exist")
-
-    context = {
-        'post': post,
-    }
-    common_context = get_common_context()
-    context.update(common_context)
-
-    return render(request, 'blog/detail.html', context)
+class PostView(CommonContextMixin, DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/detail.html'
+    pk_url_kwarg = 'post_id'
