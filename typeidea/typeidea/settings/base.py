b8fbe3506f38c2b9bf83c543dd3e11ebd9aaad92
baskershu <yangshuzhi@outlook.com>
2018-01-15 20:32:34 -0800

    抽象出css静态资源单独成为一个文件

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -95,9 +95,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+THEME = "themes/default"
+
 STATIC_URL = '/static/'
 
-THEME = "themes/default"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, THEME, "static"),
+]
 
 TEMPLATES = [
     {
