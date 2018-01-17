eb382c94f04954dbc564c5936abdd3dd4a6490fc
baskershu <yangshuzhi@outlook.com>
2018-01-17 20:04:29 +0800

    完成写评论模块的前端美化

--- typeidea/comment/forms.py
+++ typeidea/comment/forms.py
@@ -10,7 +10,28 @@ class CommentForm(forms.ModelForm):
     content = forms.CharField(
         max_length=500,
         label="内容",
-        widget=forms.widgets.Textarea(attrs={'rows': 6, 'cols': 60})
+        widget=forms.widgets.Textarea(
+            attrs={'rows': 6, 'cols': 60, 'class': 'form-control'}
+        )
+    )
+    nickname = forms.CharField(
+        max_length=50,
+        label="昵称",
+        widget=forms.widgets.Input(
+            attrs={'class': 'form-control', }
+        )
+    )
+    email = forms.CharField(
+        label="邮箱",
+        widget=forms.widgets.EmailInput(
+            attrs={'class': 'form-control', }
+        )
+    )
+    website = forms.URLField(
+        label="网址",
+        widget=forms.widgets.URLInput(
+            attrs={'class': 'form-control', }
+        )
     )
 
     def clean_content(self):
@@ -22,4 +43,4 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['nickname', 'email', 'website', 'content']
+        fields = ['nickname', 'email', 'website', 'content', ]
